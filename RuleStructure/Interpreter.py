9441c3a79aa16b8c676858661f73b6f4dc8cfbc3
ArthurVieillevoye <artvieillevoye@gmail.com>
2022-11-17 14:39:17 +0100

    Added Flexibility

--- RuleStructure/Interpreter.py
+++ RuleStructure/Interpreter.py
@@ -5,33 +5,32 @@ import Literals
 class Interpreter:
     def saveRules(self, list_of_rules):
         with open('RuleStructure\\rules_data.pkl', 'wb') as outp:
-            pickle.dump(list_of_rules, outp, pickle.HIGHEST_PROTOCOL)
+            for r in list_of_rules:
+                pickle.dump(r, outp, pickle.HIGHEST_PROTOCOL)
     
     def readRules(self):
         with open('RuleStructure\\rules_data.pkl', 'rb') as inp:
-            return(pickle.load(inp))
+            rule1 = pickle.load(inp)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     a = Literals.Literals()
     b = Literals.Literals()
-
     a.setValue(True)
     b.setValue(False)
+ 
+    lit = []
+
+    lit.append(a)
+    lit.append(b)
 
     r1 = Rules.Rules()
-    r1.setOperator(1)
+    r1.setOperator(0)
     r1.setHead(a)
     r1.setBody(b)
 
-    r2 = Rules.Rules()
-    r2.setOperator(0)
-    r2.setHead(a)
-    r2.setBody(b)
-
-    inter = Interpreter()
-    inter.saveRules([r1, r2])
-    ru= inter.readRules()
     print('hello')
-    print(len(ru))
-    print(type(ru))
+    print(r1.interpret())
+
+    b.setValue(False)
+    print(r1.interpret())
