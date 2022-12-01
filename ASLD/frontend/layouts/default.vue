2557887943ada48c8b534d02a5dc455c561f02ae
Carlos <osunacj@gmail.com>
2022-12-01 15:43:13 +0100

    home page

--- ASLD/frontend/layouts/default.vue
+++ ASLD/frontend/layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="h-screen w-screen flex flex-col">
     <div class="flex flex-row h-full w-full absolute">
-      <div class="w-2/3 bg-red-200 h-40 w-40"><Nuxt /></div>
+      <div class="w-2/3 bg-blue-800"></div>
       <div class="w-1/3"></div>
     </div>
 
-    <!-- <Navbar /> -->
+    <Navigation />
 
     <!-- <wrapper> -->
-
+    <Nuxt />
     <!-- </wrapper> -->
   </div>
 </template>
