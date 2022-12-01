5be8f566c688097da7ffb220642079dfe04f600e
Carlos <osunacj@gmail.com>
2022-12-01 14:23:03 +0100

    nuxt and tailwind

---
+++ ASLD/frontend/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen w-screen flex flex-col">
+    <div class="flex flex-row h-full w-full absolute">
+      <div class="w-2/3 bg-red-200 h-40 w-40"><Nuxt /></div>
+      <div class="w-1/3"></div>
+    </div>
+
+    <!-- <Navbar /> -->
+
+    <!-- <wrapper> -->
+
+    <!-- </wrapper> -->
+  </div>
+</template>
+<script>
+export default { name: "MainLayout" };
+</script>
+
+<style></style>
