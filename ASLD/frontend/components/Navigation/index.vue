b68e6f90d402a37f5053aa2109405d3d93ff4e7d
Carlos <osunacj@gmail.com>
2022-12-09 07:44:58 +0100

    nav

--- ASLD/frontend/components/Navigation/index.vue
+++ ASLD/frontend/components/Navigation/index.vue
@@ -2,14 +2,14 @@
   <div
     class="w-full h-18 bg-white/80 shadow backdrop-blur-md flex flex-between justify-between px-4 py-2"
   >
-    <div class="w-2/3 flex flex-row justify-between">
-      <div class="w-56">
-        <img class="scale-x-60 scale-y-25" src="~/assets/logo.png" />
-      </div>
-
-      <span class="font-bold text-3xl align-middle pt-2">ASLD</span>
+    <!-- <div class="flex flex-row justify-between"> -->
+    <div class="w-56">
+      <img class="scale-x-60 scale-y-25" src="~/assets/logo.png" />
     </div>
-    <div></div>
+
+    <span class="font-bold text-3xl pt-2">ASLD</span>
+    <!-- </div> -->
+    <!-- <div></div> -->
   </div>
 </template>
 
