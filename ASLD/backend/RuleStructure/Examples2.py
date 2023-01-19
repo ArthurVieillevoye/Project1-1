8b3bc5f8b0a220b54af47df32c39f3d6424c91ed
l-haag <leon.haag@gmail.com>
2023-01-19 12:23:51 +0100

    Example2

---
+++ ASLD/backend/RuleStructure/Examples2.py
@@ -0,0 +1,13 @@
+
+toy_examples = [
+    {
+        "id": 1,
+        "literal": "republican",
+        "question": "Is the person Republican?"
+    },
+    {
+        "id": 2,
+        "literal": "quaker",
+        "question": "Is the person quaker?"
+    },
+]
